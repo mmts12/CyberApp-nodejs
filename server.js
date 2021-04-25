3576f5571d2b46dbc8d0002bb73ab7ec3097a431
mmts12 <56490587+mmts12@users.noreply.github.com>
2021-04-25 11:56:13 +0300

    first

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.urlencoded({ extended: true })) //body parser
 
 if (process.env.NODE_ENV === 'production') {
     // app.use(express.static(path.resolve(__dirname, 'public')))
-    app.use(express.static('public'));
+    app.use(express.static(path.join(__dirname, 'public')))
+    // app.use(express.static('public'));
 } else {
     const corsOptions = {
         origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
@@ -25,7 +26,9 @@ if (process.env.NODE_ENV === 'production') {
 const attackRoutes = require('./api/attack/attack.routes');
 app.use('/api/attack', attackRoutes);
 
-app.get('/**', (req, res) => {
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
