81263e6de1044e61bb696eb13721ae2aa925400a
mmts12 <56490587+mmts12@users.noreply.github.com>
2021-04-25 12:21:31 +0300

    first

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ if (process.env.NODE_ENV === 'production') {
 const attackRoutes = require('./api/attack/attack.routes');
 app.use('/api/attack', attackRoutes);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 const port = process.env.PORT || 3030;
